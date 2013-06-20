d8915f428115f4d855ee118706918190084f3ba9
Patrick Baber <patrick@p-squared.de>
2013-06-20 20:49:55 +0200

    Adding field for multiplying different currencies with a factor.

--- app/code/community/Psquared/CurrencyAdjustment/Model/Directory/Currency/Import/Webservicex.php
+++ app/code/community/Psquared/CurrencyAdjustment/Model/Directory/Currency/Import/Webservicex.php
@@ -1,17 +1,37 @@
 <?php
 class Psquared_CurrencyAdjustment_Model_Directory_Currency_Import_Webservicex extends Mage_Directory_Model_Currency_Import_Webservicex
 {
-	protected function _numberFormat($number)
-	{
-		//no modification on the base currency
-		if ($number == 1) {
-			return $number;
+	protected function _getAdjustmentArray() {
+		$adjustments = Mage::getStoreConfig('currency/currency_adjustment/adjustment_values');
+		$adjustmentLines = explode("\n", $adjustments);
+		if (count($adjustmentLines) > 0) {
+			$adjustmentArray = array();
+			foreach ($adjustmentLines as $line) {
+				$rows = explode(';', $line);
+				$currency = trim($rows[0]);
+				$factor = trim($rows[1]);
+
+				if (!empty($currency) && !empty($factor)) {
+					$adjustmentArray[$currency] = $factor;
+				}
+			}
+			return $adjustmentArray;
+		} else {
+			return false;
 		}
+
+	}
+
+	protected function _convert($currencyFrom, $currencyTo, $retry=0)
+	{
+		$number = parent::_convert($currencyFrom, $currencyTo, $retry);
 
 		//currency adjustment
 		if (Mage::getStoreConfig('currency/currency_adjustment/enable_adjustment') == 1) {
-			$adjustment = Mage::getStoreConfig('currency/currency_adjustment/adjustment_value');
-			$number = $number * $adjustment;
+			$adjustments = $this->_getAdjustmentArray();
+			if (array_key_exists($currencyTo, $adjustments)) {
+				$number *= $adjustments[$currencyTo];
+			}
 		}
 
 		//currency rounding
@@ -30,6 +50,7 @@ class Psquared_CurrencyAdjustment_Model_Directory_Currency_Import_Webservicex ex
 					$number = floor($number);
 			}
 		}
+
 		return $number;
 	}
 }
--- app/code/community/Psquared/CurrencyAdjustment/Model/RoundingMethods.php
+++ app/code/community/Psquared/CurrencyAdjustment/Model/RoundingMethods.php
@@ -5,9 +5,9 @@ class Psquared_CurrencyAdjustment_Model_RoundingMethods
     public function toOptionArray()
     {
         return array(
-            array('value' => 1, 'label'=> Mage::helper('pricerounding')->__('round')),
-            array('value' => 2, 'label'=> Mage::helper('pricerounding')->__('ceil')),
-            array('value' => 3, 'label'=> Mage::helper('pricerounding')->__('floor')),
+            array('value' => 1, 'label'=> Mage::helper('currencyadjustment')->__('round')),
+            array('value' => 2, 'label'=> Mage::helper('currencyadjustment')->__('ceil')),
+            array('value' => 3, 'label'=> Mage::helper('currencyadjustment')->__('floor')),
         );
     }
 }
